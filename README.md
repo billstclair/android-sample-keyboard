b3db8289fdd203ee0fdc771b597fd22d764855f5
Bill St. Clair <billstclair@gmail.com>
2019-07-23 15:43:11 -0400

    Add code to github-created license.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Android Sample Keyboard
+
+This is a sample keyboard coded from [instructions at Android City](https://www.androidauthority.com/lets-build-custom-keyboard-android-832362). I found the text to be confusing, so I followed the embedded video, which is at [youtu.be/8DeQk0wlFyM](https://youtu.be/8DeQk0wlFyM).
+
+The code is currently incomplete. I stopped [at 20:19](https://youtu.be/8DeQk0wlFyM?start=1219) in the video.
---
+++ app/src/main/java/com/example/samplekeyboard/ExampleKeyboard.java
@@ -0,0 +1,49 @@
+package com.example.samplekeyboard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.inputmethodservice.*;
+
+public class ExampleKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
